2e4c27faeef80d6113a628ddc912d77e7badb4ed
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-15 23:59:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,14 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning in #{language}"
-end
-
-def introduction_with_language_optional(name, language)
-  puts "Hi my name is #{name} and I am learning in #{language}"
-end
-
 
 
 
